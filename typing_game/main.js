0949a166a5cbd50b7a77776bce0db4bd199b71d0
chiranz <chiranpoudyal@gmail.com>
2021-01-05 18:14:11 +0530

    Jan 5 2021 Loops, Arrays and Objects with array methods

--- typing_game/main.js
+++ typing_game/main.js
@@ -27,6 +27,51 @@ const quoteArray = [
 const spanArray = quoteArray.map((quote, i) => `<span>${quote} </span>`);
 
 quoteText.innerHTML = spanArray.join("");
-console.log(quoteText.childNodes);
 
 quoteText.childNodes[5].className = "highlight";
+
+// TODO: LOOPS AND ARRAY METHODS
+
+// const sentence1 =" prefix" + quoteArray[0] + "suffix " ;
+// const sentence2 = "prefix " + quoteArray[1] + "suffix " ;
+// .
+// .
+// .
+// .
+
+// Loop and Conditionals
+let arrayOfSentences = [];
+for (let i = 0; i < quoteArray.length; i++) {
+  const currentWord = quoteArray[i];
+  arrayOfSentences.push("prefix " + currentWord + " suffix");
+}
+
+// Array methods
+const arrayFromMap = quoteArray.map((quote) => "prefix " + quote + " suffix");
+
+let numbers = [];
+for (let i = 1; i < 101; i++) {
+  numbers.push(i);
+}
+const evenNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenNumbers.push(numbers[i]);
+  }
+}
+
+console.log(evenNumbers);
+
+// console.log({ arrayFromMap });
+// console.log({ arrayOfSentences });
+
+// const user = {
+//   name: "something",
+//   image: "url",
+//   tagline: "some tag",
+//   email: "some@email",
+// };
+
+// const arrayUser = ["something", "url", "some tag", "some@email"];
+// arrayUser[1];
+// user.image;
